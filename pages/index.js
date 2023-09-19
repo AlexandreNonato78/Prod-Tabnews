ca56ebef0713d30decea1a5d77781a4ec5f562e6
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 16:08:14 +0000

    bora

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Meu nome é Alexandre! Olá! </h1>
-            <img src="/workspaces/Prod-Tabnews/pages/MEME4.png" alt="MEME4" />
+            <img src="/workspaces/Prod-Tabnews/pages" alt="MEME4" />
         </div>
     );
    }
