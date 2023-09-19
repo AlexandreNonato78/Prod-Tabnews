d306e36a93753c530077cf86f969d3bdd1647d89
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 19:31:38 +0000

    modificado ao quadrado

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Meu nome é Alexandre! Olha que legal esse MEME! </h1>
+            <h2>Meu nome é Alexandre! Olha que legal esse MEME! </h2>
 
                 <img src="https://drive.google.com/file/d/1KM5aQAe-vkeIdnIq-I350zUVEtGNS5X-/view?usp=sharing" alt="MEME4" />
         </div>
