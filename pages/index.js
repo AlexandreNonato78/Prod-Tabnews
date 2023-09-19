e86484c783d33493eae00d2e050e6e280a69f98f
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 16:14:13 +0000

    vai

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ function Home() {
     return (
         <div>
             <h1>Meu nome é Alexandre! Olá! </h1>
-            <img src="/workspaces/Prod-Tabnews/pages" alt="MEME4" />
+            <img
+                src="/workspaces/Prod-Tabnews/pages"
+                alt="MEME4"
+                width="610"
+                height="430"
+            />
         </div>
     );
-   }
+}
 
 export default Home;
