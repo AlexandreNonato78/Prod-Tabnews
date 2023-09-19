66677a84528cdaeaebb2855c397f0f1f964d2dd3
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 16:42:54 +0000

    modificado

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
         <div>
             <h1>Meu nome é Alexandre! Olá! </h1>
             <img
-                src="/workspaces/Prod-Tabnews/pages"
+                src="/workspaces/Prod-Tabnews/pages/MEME4.png"
                 alt="MEME4"
                 width="610"
                 height="430"
