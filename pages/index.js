2f4eb1c31541e26c1a0fa3a6b3a9e242469ca3f1
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 15:41:17 +0000

    modificado

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Meu nome é Alexandre! Olá! </h1>
-            <img src="C:\dev" alt="MEME2" />
+            <img src="C:\dev" alt="MEME3" />
         </div>
     );
    }
