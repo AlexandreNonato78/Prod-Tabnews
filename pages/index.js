2092eaab6d96cb93f98289b1291476443d70fe30
Alexandre Nonato <101572814+AlexandreNonato78@users.noreply.github.com>
2023-09-19 19:46:26 +0000

    agora vai!

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ function Home() {
         <div>
             <h1>Meu nome é Alexandre! Olha que legal esse MEME! </h1>
 
-                <img src="https://drive.google.com/uc?export=download&id=1KM5aQAe-vkeIdnIq-I350zUVEtGNS5X-" alt="MEME4" />
+                <img src="https://drive.google.com/uc?export=download&id=1KM5aQAe-vkeIdnIq-I350zUVEtGNS5X-"
+                alt="MEME4"
+                width="610"
+                height="430"
+                />
         </div>
     );
 }
